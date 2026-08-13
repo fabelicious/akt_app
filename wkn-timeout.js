bbc68dacaa3d9a58d4c68bc1b63b01c38ab72e9f
fabelicious <sp-am@live.at>
2026-08-13 10:52:23 +0200

    Fix WKN lookup timeout and prevent stalled requests

--- wkn-timeout.js
+++ wkn-timeout.js
@@ -1,1 +1,14 @@
-(function(){'use strict';const nativeFetch=window.fetch.bind(window);window.fetch=function(input,init){const url=typeof input==='string'?input:(input&&input.url)||'';if(url.indexOf('api.openfigi.com/v3/mapping')!==-1){const timeout=new Promise((_,reject)=>setTimeout(()=>reject(new Error('OpenFIGI Timeout')),7000));return Promise.race([nativeFetch(input,init),timeout]);}return nativeFetch(input,init);};})();
+(function(){'use strict';
+  const nativeFetch=window.fetch.bind(window);
+  window.fetch=function(input,init){
+    const url=typeof input==='string'?input:(input&&input.url)||'';
+    if(url.indexOf('api.openfigi.com/v3/mapping')!==-1){
+      const c=new AbortController();
+      const originalSignal=init&&init.signal;
+      const timer=setTimeout(()=>c.abort(),1800);
+      const opts=Object.assign({},init||{}, {signal:c.signal});
+      return nativeFetch(input,opts).finally(()=>clearTimeout(timer));
+    }
+    return nativeFetch(input,init);
+  };
+})();
